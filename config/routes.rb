6c9cd67c580c8c5ed36942f012e39da682464dfc
Alex <alexandreliskiewicz@hotmail.com>
2021-07-02 15:12:06 +0200

    adding jobs controller, jobs#index route and jobs view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'static/home'
 
+  get '/jobs', to: 'jobs#index'
+
   root to: "static#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
 end
