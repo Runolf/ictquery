afd6aa1b92a6840f387eb1cc1839766915c4b37c
LotteMoors <lotte-moors@hotmail.com>
2021-07-07 16:37:41 +0200

    update homepage

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -13,19 +13,46 @@ module.exports = {
       transparent: 'transparent',
       current: 'currentColor',
       gray: colors.trueGray,
+      coolGray: colors.coolGray,
+      warmGray: colors.warmGray,
       red: colors.red,
+      orange: colors.orange,
       blue: colors.sky,
+      teal: colors.teal,
       yellow: colors.amber,
       green: colors.green,
       lime: colors.lime,
       white: colors.white,
       black: colors.black
-    }
+    },
+    keyframes: {
+      'fade-in-down': {
+          '0%': {
+              opacity: '0',
+              transform: 'translateY(-10px)'
+          },
+          '100%': {
+              opacity: '1',
+              transform: 'translateY(0)'
+          },
+      },
+      roll: {
+        '0%, 100%': { transform: 'translateX(0) rotate(0deg)' },
+        '50%': { transform: 'translateX(20rem) rotate(385deg)' }
+      }
+  },
+  animation: {
+      'fade-in-down': 'fade-in-down 1s ease-out',
+      roll: 'roll 3s ease-in-out infinite'
+  }
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ['checked'],
+      borderColor: ['checked'],
+    },
   },
   plugins: [
-    
+    require('@tailwindcss/custom-forms'),
   ],
 }
