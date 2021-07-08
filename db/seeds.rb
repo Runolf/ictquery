00fbd3a788008060be264ac89652f3cdffd8f170
martinlej <mlej7498@gmail.com>
2021-07-08 14:42:54 +0200

    Ending register/login forms

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,7 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
-#	Entreprise.create(name: 'Qiwie', about: 'Qiwie Entreprise', contactMail: 'qiwie@info.be', contactPhone: '+32 493 544 39')
-#	Job.create(name: 'Java backend developper', expectations: 'JAVA 8, Junit 4, Thymleaf', description: 'Create the new Facebook', entreprises_id: 1)
-#	Job.create(name: 'React developper', expectations: 'Using React for 8years minimum', description: 'Create the new Facebook', entreprises_id: 1)
+	Entreprise.create(name: 'Qiwie', about: 'Qiwie Entreprise', contactMail: 'qiwie@info.be', contactPhone: '+32 493 544 39')
+	Job.create(name: 'Java backend developper', expectations: 'JAVA 8, Junit 4, Thymleaf', description: 'Create the new Facebook', entreprises_id: 1)
+	Job.create(name: 'React developper', expectations: 'Using React for 8years minimum', description: 'Create the new Facebook', entreprises_id: 1)
