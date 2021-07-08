ab655f513b811bf46357e820d4f714269f363ebc
MikailMilissen <67338349+MikailMilissen@users.noreply.github.com>
2021-07-08 16:25:27 +0200

    update profile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
-#	Entreprise.create(name: 'Qiwie', about: 'Qiwie Entreprise', contactMail: 'qiwie@info.be', contactPhone: '+32 493 544 39')
-#	Job.create(name: 'Java backend developper', expectations: 'JAVA 8, Junit 4, Thymleaf', description: 'Create the new Facebook', entreprises_id: 1)
-#	Job.create(name: 'React developper', expectations: 'Using React for 8years minimum', description: 'Create the new Facebook', entreprises_id: 1)
+
+Entreprise.create(name: 'Qiwie', about: 'Qiwie Entreprise', contactMail: 'qiwie@info.be', contactPhone: '+32 493 544 39')
+Job.create(name: 'Java backend developper', expectations: 'JAVA 8, Junit 4, Thymleaf', description: 'Create the new Facebook', entreprises_id: 1)
+Job.create(name: 'React developper', expectations: 'Using React for 8years minimum', description: 'Create the new Facebook', entreprises_id: 1)
