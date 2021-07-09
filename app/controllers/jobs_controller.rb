bc25f321a03428a5dcd3707c5bcaf36f7d04dd74
Alex <alexandreliskiewicz@hotmail.com>
2021-07-09 16:23:48 +0200

    adding for loop to retrive all the checkboxes and get their value. Added new style

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,34 +1,27 @@
 class JobsController < ApplicationController
     protect_from_forgery with: :null_session #avoid the need to have a token
     def index
-        #airlines = Airline.all 
-        #render json: AirlineSerializer.new(airlines, options).serialized_json
         @jobs = Job.all 
         @entreprises = Entreprise.all
 
+        
+
         idE = @jobs[0].entreprises_id
         @enter = Entreprise.find(idE)
+
+        # 1: get all entreprises
+        # 2: get the jobs by entreprises 
+
 
         
     end
 
     def show
-        #airline  = Airline.find_by(slug: params[:slug])
-        #render json: AirlineSerializer.new(airline, options).serialized_json
         @job = Job.find(params[:id])
     end
 
     def create
-        #airline = Airline.new(airline_params)
-        #if airline.save
-        #    render json: AirlineSerializer.new(airline).serialized_json
-        #else
-        #   render json: {error: airline.errors.messages }, status: 422
-        #end            
-        
         job = Job.create(name: 'test Name', expectations: 'expec test', description: 'desc test')
-
-     
     end
 
     def update
