ca49f86564b51c7cae65e3951c9535c7ba85b337
Alex <alexandreliskiewicz@hotmail.com>
2021-08-12 16:51:00 +0200

    add image on Qiwie and Aubay company + display those images on jobs page

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,9 @@
 #Adresse Qiwie
 #Adress.create(street: "Bd Saint-Lazare", city: "Saint-Josse-ten-Noode, Bruxelles", postalCode: "1210", country: "Belgium", houseNumber: "4/10", googleMapUrl: "https://goo.gl/maps/LJqu5wZUwr8s1J9r8")
 
-Contracttype.create(name: "full time")
-Contracttype.create(name: "Part time")
-Contracttype.create(name: "Half time")
+#Contracttype.create(name: "full time")
+#Contracttype.create(name: "Part time")
+#Contracttype.create(name: "Half time")
 
 #Experience.create(name: "Junior")
 #Experience.create(name: "Medior")
@@ -19,20 +19,26 @@ Contracttype.create(name: "Half time")
 #Job.create(name: 'PHP senior developper', expectations: 'Laravel, Senior', description: 'Netflix but better', entreprise_id: 4,  adresse_id: 2, experience_id: 2)
 #Job.create(name: 'Angular medior developper', expectations: 'Using Angular for 4 years minimum', description: 'Netflix but better', entreprise_id: 4,  adresse_id: 2, experience_id: 3)
 
-junior = Experience.find_by_id(1)
-medior = Experience.find_by_id(2)
-senior = Experience.find_by_id(3)
+#junior = Experience.find_by_id(1)
+#medior = Experience.find_by_id(2)
+#senior = Experience.find_by_id(3)
 # junior.update(name: "junior")
 # medior.update(name: "medior")
 # senior.update(name: "senior")
 
-job7 = Job.find_by_id(7) 
-job8 = Job.find_by_id(8)
-job10 = Job.find_by_id(10)
-job11 = Job.find_by_id(11)
+#job7 = Job.find_by_id(7) 
+#job8 = Job.find_by_id(8)
+#job10 = Job.find_by_id(10)
+#job11 = Job.find_by_id(11)
 
 #job7.update(experience_id: 2, contracttype_id: 1)
 #job8.update(experience_id: 3, contracttype_id: 1)
 #job10.update(experience_id: 3, contracttype_id: 1)
 # job11.update(experience_id: 1, contracttype_id: 3)
+
+#qiwie = Entreprise.find_by_id(5)
+#qiwie.update(logo: "logoQiwie.png")
+
+#aubay = Entreprise.find_by_id(4)
+#aubay.update(logo: "AubayLogo.png")
 
